2a0fe84cc75f0f2464e7ddf15cd20fb651963740
bsrija <bsrija@DIN66008979.corp.capgemini.com>
2018-07-06 17:32:09 +0530

    this is a dummy code

---
+++ dumbyprojects/src/com/capg/DumbyProject.java
@@ -0,0 +1,21 @@
+package com.capg;
+
+public class DumbyProject {
+
+	public static void main(String[] args) {
+		System.out.println("welcome tto java");
+		System.out.println("hoo");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
